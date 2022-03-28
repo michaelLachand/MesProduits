6ad551e496970b9d3189963396bf46bebc7b9bf7
wes35 <lachand_michael@hotmail.com>
2022-03-28 10:51:14 +0200

    ajout component add-Produits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProduitsComponent
+    ProduitsComponent,
+    AddProduitComponent
   ],
   imports: [
     BrowserModule,
